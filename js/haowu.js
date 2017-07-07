d79d07b3e74d4df875cc11d63d00c2316309da08
Snap <1039146267@qq.com>
2017-07-07 20:44:24 +0800

    0707

--- js/haowu.js
+++ js/haowu.js
@@ -1,7 +1,14 @@
 //轮播图------------------------------------
 var mySwiper = new Swiper ('.swiper-container', {
-	slidesPerView : 4,
-    spaceBetween : 20,
+    direction: 'horizontal',
+    loop: true,
+    autoplay : 2000,
+	speed:500,
+    // 如果需要分页器
+    pagination: '.swiper-pagination',
+    paginationClickable :true,
+    autoplayDisableOnInteraction : false,
+    
  });
 //向下滚动 固定导航----------------------------------
 $(window).scroll(function(){
