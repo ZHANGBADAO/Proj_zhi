8da4acc77c87bb2874ce0673892df48404f9218f
Snap <1039146267@qq.com>
2017-07-13 15:07:24 +0800

    0713

---
+++ js/setting.js
@@ -0,0 +1,13 @@
+
+//向下滚动 固定导航----------------------------------
+$(window).scroll(function(){
+		let scrolltop=document.documentElement.scrollTop || document.body.scrollTop;
+		if (scrolltop>=250) {
+			$("header").css({top:"-1.6rem",transition:"0.5s"});
+			$("nav").css({top:"0",transition:"0.5s"});
+		}else{
+			$("header").css({top:"0"});
+			$("nav").css({top:"1.6rem"});
+		}
+});	
+		
